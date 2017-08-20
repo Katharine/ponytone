daa333a768b3b7d497afe19513a7d19d2ae54a79
Katharine Berry <katharine@kathar.in>
2017-08-19 18:34:37 -0700

    Song in the query string.

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,9 @@ import {LiveAudio} from "./audio/live";
 
 LiveAudio.requestPermissions();
 
-let session = new GameSession(document.getElementById('game-container'), 1280, 720, "https://s3-us-west-2.amazonaws.com/mlkonline/cupcakes/notes.txt");
+let song = location.search.substr(1) || 'cupcakes';
+
+let session = new GameSession(document.getElementById('game-container'), 1280, 720, `https://s3-us-west-2.amazonaws.com/mlkonline/${song}/notes.txt`);
 
 document.getElementById('loading-image').src = require('../img/loading.png');
 
